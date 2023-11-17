2260faf8f594badffba9e80bcc170fae9ce0e7c5
AndreaiOS <an.murru@gmail.com>
2023-11-17 09:58:22 +0000

    First commit

---
+++ TADInventoryTracker/TADInventoryTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TADInventoryTrackerApp.swift
+//  TADInventoryTracker
+//
+//  Created by Andrea Murru on 17/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct TADInventoryTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
